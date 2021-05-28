cf8f901d1b01912d6afc2d3b182120b198a56fc4
aaron-might <aaronmighty7@gmail.com>
2021-05-28 12:39:47 -0400

    update script.js drinkrecipe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,31 @@
 let rmgBtnEle = document.querySelector('button');
 
-// let mealTitleEle = document.querySelector('');
-// let mealImgEle = document.querySelector('');
-// let mealInstrucEle = document.querySelector('');
-// let mealUlEle = document.querySelector('');
-// let mealIngredEle = document.querySelector('');
+let mealTitleEle = document.querySelector('.meal-name');
+let mealImgEle = document.querySelector('.meal-img');
+let mealInstrucEle = document.querySelector('.meal-instructions');
+let mealUlEle = document.querySelector('.meal-ingredients');
+let mealLinkEle = document.querySelector('.meal-link');
 
-// let drinkTitleEle = document.querySelector('');
-// let drinkImgEle = document.querySelector('');
-// let drinkInstrucEle = document.querySelector('');
-// let drinklUlEle = document.querySelector('');
-// let drinkIngredEle = document.querySelector('');
+ let drinkTitleEle = document.querySelector('.drink-name');
+ let drinkImgEle = document.querySelector('.drink-img');
+ let drinkInstrucEle = document.querySelector('.drink-instructions');
+ let drinkUlEle = document.querySelector('.drink-ingredients');
+ let drinkLinkEle = document.querySelector('.drink-link');
 
-let getMealData = () => {
+const getMealData = () => {
     let apiUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
-    
+
     fetch(apiUrl)
-    .then(response => response.json())
-    .then(meal => {
-        // console.log(meal.meals[0])
-        displayMealRecipe(meal.meals[0]);
-    })
-    .catch(error => console.log(error)); 
+        .then(response => response.json())
+        .then(meal => {
+            // console.log(meal.meals[0])
+            displayMealRecipe(meal.meals[0]);
+        })
+        .catch(error => console.log(error));
 }
 
-// let getDrinkData = () => {
-//     let apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
-    
-//     fetch(apiUrl)
-//     .then(response => response.json())
-//     .then(drink => {
-//         // console.log(drink.meals[0])
-//         displayRecipe(drink.drinks[0]);
-//     })
-//     .catch(error => console.log(error)); 
-// }
 
-let displayMealRecipe = (meal) => {
+const displayMealRecipe = (meal) => {
     let mealImg = meal.strMealThumb;
     let numIngreds = (Object.entries(meal).filter(item => item[0].match(/Ingredient/))).filter(item => item[1]).length;
     let ingreds = [];
@@ -50,15 +39,47 @@ let displayMealRecipe = (meal) => {
         }
     }
 
-    // ingreds.map(item => {
-    //     let liEle = document.createElement('li');
-    //     liEle.textContent = item;
-    //     mealUlEle.appendChild(liEle);
-    // });
-    // mealTitleEle.textContent = meal.strMeal;
-    // mealImgEle.setAttribute('src', mealImg);
-    // mealInstrucEle.textContent = meal.strInstructions;
-
 }
 
+const getDrinkData = () => {
+    let apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(drink => {
+           // console.log(drink.drinks[0]) 
+           displayDrinkRecipe(drink.drinks[0]);
+           
+        })
+}
+
+const displayDrinkRecipe = (drink) => {
+    let drinkImg= drink.strDrinkThumb;
+    let ingreds=[];
+    for (let i=0; i<20; i++){
+        let ingred = drink[`strIngredient${i}`];
+        let meas = drink[`strMeasure${i}`];
+      //  console.log(drink[`strIngredient${i}`])
+        if (ingred){
+            ingreds.push(`${ingred}: ${meas}`.replace(/\s+/g, " "));
+        }
+    
+    }
+        console.log(ingreds)
+
+    drinkUlEle.innerHTML = " ";
+    ingreds.forEach(item => {
+        let liEle = document.createElement("li");
+        drinkUlEle.appendChild(liEle);
+        liEle.textContent = item;
+    })
+
+    drinkTitleEle.textContent= drink.strDrink;
+    drinkImgEle.setAttribute("src",drinkImg); 
+    drinkInstrucEle.textContent=drink.strInstructions;
+
+
+
+}
 rmgBtnEle.addEventListener('click', getMealData);
+rmgBtnEle.addEventListener('click', getDrinkData);

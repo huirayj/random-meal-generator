1ab4699030d15d3da357fec44b4cde9cca949447
Justin Hui <huirayj@gmail.com>
2021-05-30 13:28:57 -0400

    reviewed script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,89 +1,86 @@
 let rmgBtnEle = document.querySelector('button');
+let sectionsEle = document.querySelector('section');
 
 let mealTitleEle = document.querySelector('.meal-name');
+let mealLinkEle = document.querySelector('.meal-link');
 let mealImgEle = document.querySelector('.meal-img');
 let mealInstrucEle = document.querySelector('.meal-instructions');
 let mealUlEle = document.querySelector('.meal-ingredients');
-let mealLinkEle = document.querySelector('.meal-link');
 
- let drinkTitleEle = document.querySelector('.drink-name');
- let drinkImgEle = document.querySelector('.drink-img');
- let drinkInstrucEle = document.querySelector('.drink-instructions');
- let drinkUlEle = document.querySelector('.drink-ingredients');
- let drinkLinkEle = document.querySelector('.drink-link');
+let drinkTitleEle = document.querySelector('.drink-name');
+let drinkImgEle = document.querySelector('.drink-img');
+let drinkInstrucEle = document.querySelector('.drink-instructions');
+let drinkUlEle = document.querySelector('.drink-ingredients');
 
 const getMealData = () => {
     let apiUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
     fetch(apiUrl)
         .then(response => response.json())
-        .then(meal => {
-            // console.log(meal.meals[0])
-            displayMealRecipe(meal.meals[0]);
-        })
+        .then(meal => displayMealRecipe(meal.meals[0]))
         .catch(error => console.log(error));
 }
 
-
-const displayMealRecipe = (meal) => {
-    let mealImg = meal.strMealThumb;
-    let numIngreds = (Object.entries(meal).filter(item => item[0].match(/Ingredient/))).filter(item => item[1]).length;
-    let ingreds = [];
-
-    for (let i = 0; i <= numIngreds; i++) {
-        let ingred = meal[`strIngredient${i}`];
-        let meas = meal[`strMeasure${i}`];
-
-        if (ingred) {
-            ingreds.push(`${ingred}: ${meas}`.replace(/\s+/g, " "));
-        }
-    }
-    mealUlEle.innerHTML = '';
-    ingreds.map(item => {
-        let liEle = document.createElement('li');
-        mealUlEle.appendChild(liEle);
-        liEle.textContent = item;
-    });
-    mealTitleEle.textContent = meal.strMeal;
-    mealImgEle.setAttribute('src', mealImg);
-    mealInstrucEle.textContent = meal.strInstructions;
+const getDrinkData = () => {
+    let apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
 
     fetch(apiUrl)
         .then(response => response.json())
-        .then(drink => {
-           // console.log(drink.drinks[0]) 
-           displayDrinkRecipe(drink.drinks[0]);
-           
-        })
+        .then(drink => displayDrinkRecipe(drink.drinks[0]))
+        .catch(error => console.log(error));
+}
+
+const displayMealRecipe = (meal) => {
+    let mealIngreds = [];
+
+    for (let i = 0; i <= 20; i++) {
+        let ingred = meal[`strIngredient${i}`];
+        let meas = meal[`strMeasure${i}`];
+
+        ingred && mealIngreds.push(`${ingred}: ${meas}`);
+    }
+
+    mealUlEle.innerHTML = '';
+    mealIngreds.forEach(item => {
+        let liEle = document.createElement('li');
+
+        mealUlEle.appendChild(liEle);
+        liEle.textContent = item;
+    });
+
+    mealImgEle.setAttribute('src', meal.strMealThumb);
+    mealLinkEle.setAttribute('href', meal.strSource)
+    mealTitleEle.textContent = meal.strMeal;
+    mealInstrucEle.textContent = meal.strInstructions;
 }
 
 const displayDrinkRecipe = (drink) => {
-    let drinkImg= drink.strDrinkThumb;
-    let ingreds=[];
-    for (let i=0; i<20; i++){
+    let drinkIngreds = [];
+
+    for (let i = 0; i <= 15; i++) {
         let ingred = drink[`strIngredient${i}`];
         let meas = drink[`strMeasure${i}`];
-      //  console.log(drink[`strIngredient${i}`])
-        if (ingred){
-            ingreds.push(`${ingred}: ${meas}`.replace(/\s+/g, " "));
-        }
-    
-    }
-        console.log(ingreds)
 
-    drinkUlEle.innerHTML = " ";
-    ingreds.forEach(item => {
-        let liEle = document.createElement("li");
+        ingred && drinkIngreds.push(`${ingred}: ${meas}`);
+    }
+
+    drinkUlEle.innerHTML = '';
+    drinkIngreds.forEach(item => {
+        let liEle = document.createElement('li');
+
         drinkUlEle.appendChild(liEle);
         liEle.textContent = item;
-    })
+    });
 
-    drinkTitleEle.textContent= drink.strDrink;
-    drinkImgEle.setAttribute("src",drinkImg); 
-    drinkInstrucEle.textContent=drink.strInstructions;
-
-
-
+    drinkImgEle.setAttribute('src', drink.strDrinkThumb);
+    drinkTitleEle.textContent = drink.strDrink;
+    drinkInstrucEle.textContent = drink.strInstructions;
 }
-rmgBtnEle.addEventListener('click', getMealData);
-rmgBtnEle.addEventListener('click', getDrinkData);
+
+const init = () => {
+    sectionsEle.classList.remove('hidden');
+    getMealData();
+    getDrinkData();
+}
+
+rmgBtnEle.addEventListener('click', init);
